fcb4a8bc52125512fda7bc2f1f23e04c36093b32
Blair Azzopardi <blairuk@gmail.com>
2023-05-17 00:29:26 +0100

    Initial build script.

---
+++ build.py
@@ -0,0 +1,24 @@
+from mypyc.build import mypycify
+
+
+modules = [
+    "yabte/backtest/__init__.py",
+    "yabte/backtest/_helpers.py",
+    "yabte/backtest/asset.py",
+    "yabte/backtest/book.py",
+    "yabte/backtest/order.py",
+    "yabte/backtest/strategy.py",
+    "yabte/backtest/transaction.py",
+]
+
+extensions = mypycify(modules)
+
+
+def build(setup_kwargs):
+    """Needed for the poetry building interface."""
+
+    setup_kwargs.update(
+        {
+            "ext_modules": extensions,
+        }
+    )
